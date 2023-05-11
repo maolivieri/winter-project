acdb2c8ce0f0c69ef3c189b09ca6676ac1680df0
Matheus Olivieri <matheus.olivieri@yahoo.com>
2023-05-11 17:31:48 -0300

    test: first e2e test for user creation

--- src/http/controllers/system/user/create.spec.ts
+++ src/http/controllers/system/user/create.spec.ts
@@ -1,6 +1,21 @@
+import { app } from '@/app'
+import request from 'supertest'
 
-test('ATESTSTASTASTASTAS', () => {
-  const a = 1
+describe('create user (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+  afterAll(async () => {
+    await app.close()
+  })
 
-  expect(a).toBe(1)
+  it('should be able to create a user', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456'
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
 })
